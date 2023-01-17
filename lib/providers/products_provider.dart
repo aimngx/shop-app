1a194d4ff7c2ca090e900893ea9a9bd28086759a
Aiman <aimanhakimsupee@gmail.com>
2023-01-18 07:42:02 +0800

    provider and listener

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -3,7 +3,7 @@ import 'package:shop_app/models/product.dart';
 
 //create mixin to merge with other class
 class Products with ChangeNotifier {
-  List<Product> _items = [
+  final List<Product> _items = [
     Product(
       id: 'p1',
       title: 'Red Shirt',
@@ -40,6 +40,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items {
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
   }
 
   void addProduct() {
